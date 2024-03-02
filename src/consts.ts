d040b521c751ecb774c96401277ac42509c87bb0
leobema <leonardoberti21@gmail.com>
2024-03-02 20:03:48 -0300

    Agregar cambios

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Todo Regalo';
+export const SITE_DESCRIPTION = 'Encuentra regalos perfectos para todas las ocasiones en Tienda Online. Ideas cuidadosamente seleccionadas para sorprender a pareja, hijos, amigos y familiares. Desde detalles románticos hasta opciones únicas y originales, nuestro equipo te ofrece consejos para crear momentos especiales. Haz clic ahora para explorar.';
